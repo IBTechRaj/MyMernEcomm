87aec4908ffec996c8094b16993bd4043c3f544f
ibtechraj <krs30018@gmail.com>
2024-07-16 11:21:36 +0530

    fixing deployment errors

--- backend/controller/product/getCategoryProductOne.js
+++ backend/controller/product/getCategoryProductOne.js
@@ -3,39 +3,39 @@ const productModel = require("../../models/productModel")
 
 const getCategoryProduct = async (req, res) => {
     console.log("category 1", productCategory)
-    try {
+    // try {
 
-        const productCategory = await productModel.distinct("category")
+    const productCategory = await productModel.distinct("category")
 
-        console.log("category 2", productCategory)
+    console.log("category 2", productCategory)
 
-        //array to store one product from each category
-        const productByCategory = []
+    //array to store one product from each category
+    const productByCategory = []
 
-        for (const category of productCategory) {
-            const product = await productModel.findOne({ category })
+    for (const category of productCategory) {
+        const product = await productModel.findOne({ category })
 
-            if (product) {
-                productByCategory.push(product)
-            }
+        if (product) {
+            productByCategory.push(product)
         }
-
-
-        res.json({
-            message: "category product",
-            data: productByCategory,
-            success: true,
-            error: false
-        })
-
-
-    } catch (err) {
-        res.status(400).json({
-            message: err.message || err,
-            error: true,
-            success: false
-        })
     }
+
+
+    res.json({
+        message: "category product",
+        data: productByCategory,
+        success: true,
+        error: false
+    })
+
+
+    // } catch (err) {
+    //     res.status(400).json({
+    //         message: err.message || err,
+    //         error: true,
+    //         success: false
+    //     })
+    // }
 }
 
 module.exports = getCategoryProduct
