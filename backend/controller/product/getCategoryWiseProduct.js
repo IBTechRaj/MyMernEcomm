7b06e49dcd8c0e335d013ac4dfe7da529ee2bae9
ibtechraj <krs30018@gmail.com>
2024-07-16 10:54:57 +0530

    fixing deployment errors

--- backend/controller/product/getCategoryWiseProduct.js
+++ backend/controller/product/getCategoryWiseProduct.js
@@ -2,24 +2,26 @@ const productModel = require("../../models/productModel")
 
 const getCategoryWiseProduct = async (req, res) => {
     console.log("catwise 1")
-    try {
+    console.log("req", req.body)
+    console.log("req2", req.query)
+    // try {
 
-        const { category } = req?.body || req?.query
-        const product = await productModel.find({ category })
-        console.log("catwise 2")
-        res.json({
-            data: product,
-            message: "Product",
-            success: true,
-            error: false
-        })
-    } catch (err) {
-        res.status(400).json({
-            message: err.message || err,
-            error: true,
-            success: false
-        })
-    }
+    const { category } = req?.body || req?.query
+    const product = await productModel.find({ category })
+    console.log("catwise 2")
+    res.json({
+        data: product,
+        message: "Product",
+        success: true,
+        error: false
+    })
+    // } catch (err) {
+    //     res.status(400).json({
+    //         message: err.message || err,
+    //         error: true,
+    //         success: false
+    //     })
+    // }
 }
 
 module.exports = getCategoryWiseProduct
