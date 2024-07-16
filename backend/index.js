d181f61766195089902375b54a5455a1bf57c620
ibtechraj <krs30018@gmail.com>
2024-07-16 06:55:05 +0530

    fixing deployment errors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ const app = express()
 app.use(cors(
     {
         // origin: 'http://localhost:3000',
-        origin: 'https://mymernecomm-frontend.onrender.com',
+        // origin: 'https://mymernecomm-frontend.onrender.com',
+        origin: "*",
         credentials: true
     }
 ))
