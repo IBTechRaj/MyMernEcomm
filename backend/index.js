0c3f7f9cbc0da16006b4456e80d39edbaff87866
ibtechraj <krs30018@gmail.com>
2024-07-23 16:49:16 +0530

    debugging login

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ const router = require('./routes')
 const app = express()
 app.use(cors(
     {
-        origin: 'http://localhost:3000',
-        // origin: 'https://mymernecomm-frontend.onrender.com',
+        // origin: 'http://localhost:3000',
+        origin: 'https://mymernecomm-frontend.onrender.com',
         credentials: true
     }
 ))
