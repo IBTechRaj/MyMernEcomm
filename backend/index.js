2c5b26fcf939d94e6e2545c1df2b1d1c8f7f1fd3
ibtechraj <krs30018@gmail.com>
2024-07-24 09:44:41 +0530

    debugging login

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
-
+const session = require("express-session");
 
 const app = express()
 app.use(cors(
