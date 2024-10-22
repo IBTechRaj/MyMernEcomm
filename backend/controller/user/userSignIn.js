007f84db911b52f39f3ec913b2c557ff8c51c327
ibtechraj <krs30018@gmail.com>
2024-10-22 22:37:27 +0530

    debug login

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -33,7 +33,7 @@ async function userSignInController(req, res) {
             const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
 
             const tokenOption = {
-                httpOnly: true,
+                httpOnly: false,
                 // sameSite: "none",
                 secure: true
             }
