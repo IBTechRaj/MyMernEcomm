ef2af45e39a3d514dc24ffb7e461c633cfefa11d
ibtechraj <krs30018@gmail.com>
2024-10-24 19:41:56 +0530

    dubg login

--- frontend/src/helpers/addToCart.js
+++ frontend/src/helpers/addToCart.js
@@ -28,7 +28,7 @@ const addToCart = async (e, id) => {
     )
 
     // const responseData = await response.json()
-
+    console.log('addtocart', responseData)
     if (responseData.success) {
         toast.success(responseData.message)
     }
